728013fc33158843770bc69b49a8e35f55b637df
My-Name-Is-Nabil <midorady9999@gmail.com>
2021-04-23 01:32:05 +0200

    Ready

--- js/index.js
+++ js/index.js
@@ -15,7 +15,25 @@ navItems.click( event =>{
     else    
         tabs.addClass('third');
 })
-
+$('.header__burgers').click(()=>{
+    $('.mobile-menu').show(200,()=>{});
+});
+$('.mobile-menu__close').click(()=>{
+    $('.mobile-menu').hide(200,()=>{});
+});
+$('#contact-us').click(()=>{
+    console.log($('#email').val());
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test($('#email').val())){
+        $('#email').css('background-image',"none");
+        $('#email').css('border','none');
+        $('form').removeClass('show');
+    }
+    else{
+        $('#email').css('background-image','url("./images/icon-error.svg")');
+        $('#email').css('border','2px #fa5757 solid');
+        $('form').addClass('show');
+    }
+});
 const stopResponsiveTransition = () =>{
     let timer = null;
     window.addEventListener('resize', function () {
